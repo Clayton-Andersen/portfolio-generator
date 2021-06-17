31b7cc14ae57773af45a15670882386baef91fa4
Aloha <utah.clayton@gmail.com>
2021-06-17 07:18:14 -0700

    start generate page

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+//9.2.4 bottom of page create html
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 //why did lesson snapshot only show 'const progileDataArgs = process.argv.slice(2);'?
-
-const printProfileData = profileDataArr => {
-    //This...
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('=====================');
+const [name, github] = profileDataArgs;
+// const printProfileData = profileDataArr => {
+//     //This...
+//     for (let i=0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('=====================');
     
-    //Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem)) 
-    }
-};
+//     //Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem)) 
+//     }
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+const generatePage = (userName, githubName) => {
+    return`
+    Name: ${userName}, 
+    Github: ${githubName}
+    `;
+};
+console.log(generatePage(name, github));
