a91025300912c78fc0a9b326ce7cd50293ebb83e
Aloha <utah.clayton@gmail.com>
2021-06-17 23:15:34 -0700

    generated html, next step is refactor

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-//9.2.4 bottom of page create html
+//top of 9.2.6
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 //why did lesson snapshot only show 'const progileDataArgs = process.argv.slice(2);'?
 const [name, github] = profileDataArgs;
-// const printProfileData = profileDataArr => {
-//     //This...
-//     for (let i=0; i < profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('=====================');
-    
-//     //Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem)) 
-//     }
-// };
 
-// printProfileData(profileDataArgs);
-
-const generatePage = (userName, githubName) => {
+const generatePage = (name, github) => {
     return`
-    Name: ${userName}, 
-    Github: ${githubName}
+    <!DOCTYPE html>
+    <html land="en">
+    <head>
+        <meta charset="UTF-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+    
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>     
     `;
 };
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name,github), err => {
+    if (err) throw err; 
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
