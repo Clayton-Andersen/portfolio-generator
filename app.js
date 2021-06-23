25ecb6c51469ed2879d8d31e9b892915d55e6e3d
Aloha <utah.clayton@gmail.com>
2021-06-22 17:56:17 -0700

    installed inquirer

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-//top of 9.2.7
+const inquirer = require('inquirer');
+// 9.3.5 From this example, let's mimic the same pattern and add it to the app.js file, as shown here:
 
-const fs = require('fs');
-const generatePage = require(`./src/page-template.js`);
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require(`./src/page-template.js`);
+
+// const pageHtTML = generatePage(name, github);
 
 
-fs.writeFile('index.html', generatePage(name,github), err => {
-    if (err) throw new Error (err); 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// fs.writeFile('index.html', generatePage(name,github), err => {
+//     if (err) throw new Error (err); 
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
