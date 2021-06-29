93e9ba6993472d906dfcc658f424a33e4e1b58a9
Aloha <utah.clayton@gmail.com>
2021-06-29 16:08:52 -0700

    updated template to style featured and non featured projects and write html

--- app.js
+++ app.js
@@ -124,14 +124,15 @@ const promptProject = portfolioData => {
         });
 };
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
     });
-// // const pageHtTML = generatePage(name, github);
+  });
 
 
-// // fs.writeFile('index.html', generatePage(name,github), err => {
-// //     if (err) throw new Error (err); 
-// //     console.log('Portfolio complete! Check out index.html to see the output!')
-// // });
